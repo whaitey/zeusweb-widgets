e20c90f51473c8995650c01f50e8672ec7924064
whaitey <nincsilyenxx@gmail.com>
2025-07-28 19:46:16 +0200

    Update author to ZeusWeb in plugin headers and README

--- zeusweb-widgets.php
+++ zeusweb-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: ZeusWeb Widgets
  * Description: Custom Elementor widgets for ZeusWeb.
  * Version: 1.0.0
- * Author: Your Name
+ * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/zeusweb-widgets
  * GitHub Plugin URI: https://github.com/whaitey/zeusweb-widgets
  * GitHub Branch: master
