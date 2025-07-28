67d4ae32a8b82e9eb3a5298f690fa3de3a9cb2ac
whaitey <nincsilyenxx@gmail.com>
2025-07-28 19:43:31 +0200

    Add GitHub update headers for automatic updates

--- zeusweb-widgets.php
+++ zeusweb-widgets.php
@@ -2,8 +2,16 @@
 /**
  * Plugin Name: ZeusWeb Widgets
  * Description: Custom Elementor widgets for ZeusWeb.
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Your Name
+ * Plugin URI: https://github.com/whaitey/zeusweb-widgets
+ * GitHub Plugin URI: https://github.com/whaitey/zeusweb-widgets
+ * GitHub Branch: master
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.0
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
