052d3e846b45b6604530eedfce68ca58da973077
Alex Kiss <alexkiss@Alexs-MacBook-Air.local>
2025-08-06 14:27:37 +0200

    v1.2.6: Fix Kiállítók widget layout with important overrides to ensure proper vertical stacking and 200px image size

--- zeusweb-widgets.php
+++ zeusweb-widgets.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ZeusWeb Widgets
  * Description: Custom Elementor widgets for ZeusWeb.
- * Version: 1.2.5
+ * Version: 1.2.6
  * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/zeusweb-widgets
  * GitHub Plugin URI: https://github.com/whaitey/zeusweb-widgets
