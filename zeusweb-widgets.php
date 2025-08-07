a76b7a26ce852369207cee4823d4fdf15ee9f759
Alex Kiss <alexkiss@Alexs-MacBook-Air.local>
2025-08-07 13:10:53 +0200

    v1.4.5: Fix Kiállítók pagination dots color - change from gray to white

--- zeusweb-widgets.php
+++ zeusweb-widgets.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ZeusWeb Widgets
  * Description: Custom Elementor widgets for ZeusWeb.
- * Version: 1.4.4
+ * Version: 1.4.5
  * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/zeusweb-widgets
  * GitHub Plugin URI: https://github.com/whaitey/zeusweb-widgets
