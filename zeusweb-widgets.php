8418c9c60cb078cf172044cf40eefb8f5be8efb4
Alex Kiss <alexkiss@Alexs-MacBook-Air.local>
2025-09-09 12:57:21 +0200

    v1.5.9: Bump version (GYIK two-column enforcement)

--- zeusweb-widgets.php
+++ zeusweb-widgets.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ZeusWeb Widgets
  * Description: Custom Elementor widgets for ZeusWeb.
- * Version: 1.5.8
+ * Version: 1.5.9
  * Author: ZeusWeb
  * Plugin URI: https://github.com/whaitey/zeusweb-widgets
  * GitHub Plugin URI: https://github.com/whaitey/zeusweb-widgets
